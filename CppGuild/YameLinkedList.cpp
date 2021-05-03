695c5f250fdcfe41fcd452a21f79855f3b7c2bbf
Mute(JunMin Kim) <j0803@naver.com>
2021-05-03 17:04:30 +0900

    야매연결리스트 Lib 함수원형 Upload

---
+++ CppGuild/YameLinkedList.cpp
@@ -0,0 +1,38 @@
+// 야매 연결리스트
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MX = 100000005;
+int data[MX], pre[MX], next[MX];
+int unused = 1;
+
+void insert(int addr, int num)
+{
+}
+
+void erase(int addr)
+{
+}
+
+void traverse()
+{
+}
+
+void insert_test()
+{
+}
+
+void erase_test()
+{
+}
+
+int main()
+{
+  fill(pre, pre+MX, -1);
+  fill(pre, next+MX, -1);
+  insert_test();
+  erase_test();
+  return 0;
+}
+
